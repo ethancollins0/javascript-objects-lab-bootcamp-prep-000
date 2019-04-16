f7efa9f70c5fb511d0f26b759f6346a497874e55
Ethan Collins <ethantcollins98@gmail.com>
2019-04-16 00:28:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return object = Object.assign({key: value})
+  return object = Object.assign({}, recipes, {key: value})
 }
